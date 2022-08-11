c119d6eeac6cb6fa2cb1322249a1a949605b9799
JuanfranZZ <55857618+JuanfranZZ@users.noreply.github.com>
2022-08-11 23:16:41 +0200

    Update potential.py
    checkpoint potential

--- potential.py
+++ potential.py
@@ -4,34 +4,39 @@ from matplotlib import pyplot as plt
 
 
 def V_r(r, m):
-    G = 6.6738480 * 10 ** (-11)  # Nm^2/Kg^2
-    return -G * m / r
+
+    return -m / r
 
 
 def V_no_m(r, _):
     return -1 / r
 
 
-def V_eff(l,m,r,V):
+def V_eff(l, m, r, V):
     return l*l/(2*m*r*r)+V
 
 
-def V_Yukawa(r, _):
-    return -(np.exp(-r))/r
+def V_Yukawa(r, m):
+    return -m*(np.exp(-r))/r
 
 
 def plot_V_eff():
-    r = np.linspace(0.01, 5, 100)
-    r0 = [r[0], 0.65, 0.8, 1.25]  # r[0]
-    m = 1
-    v = 1
-    l = r0*m*v
-    for ii in l:
-        y = V_eff(ii, m, r, V_no_m(r, m))
-        plt.plot(r, y)
-    plt.axis([0, 5, -1.5, 0.5])
+    phi0 = 0  # rad
+    v0 = 30.3  # km/s 7181.9  # m/s 10*np.sqrt(G*m/r0)
+    m1 = 1.989e30
+    m2 = 5.972e24
+    m = m1*m2/(m1+m2)
+    M = m1+m2
+    r0 = 147000000  # (1350+6740)# *1000 # m
+    r = np.linspace(0.001, 2*r0, 100)
+
+    l = r0*m*v0
+
+    y = V_r(r, m) + (l * l) / (2 * m * r * r)
+    plt.plot(r, y)
+    plt.axis([0, 2*r0, -1.5, 1e30])
     plt.axhline(y=0, color='black', linestyle='--')
-    plt.legend(r0)
+    #plt.legend(r0)
     plt.show()
 
 
